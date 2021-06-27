61d47cc68e7f0d84a3f0e94310a3049c6e733771
PetarVujetic <vujeticp@gmail.com>
2021-06-27 16:30:43 +0200

    Error handling

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const path = require('path')
 const colors = require('colors')
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
+const errorHandler = require('./middlewares/error')
+
 
 // Load config
 dotenv.config({ path: "./config/config.env" })
@@ -12,14 +15,24 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
-
 // Load routes
 const businessRoute = require('./routes/business')
 
+// Middlewares
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Set routes
 app.use('/api/v1/business', businessRoute)
 
+app.use(errorHandler)
+
 const server = app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold);
+})
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  //Close server and exit process
+  server.close(() => process.exit(1))
 })
