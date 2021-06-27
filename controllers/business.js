5e8e5264e757e5a806ae794334f56c1d31932851
PetarVujetic <vujeticp@gmail.com>
2021-06-27 22:34:00 +0200

    full business crud (without user permissions)

--- controllers/business.js
+++ controllers/business.js
@@ -1,6 +1,6 @@
 const asyncHandler = require('../middlewares/asyncHandler')
 const Business = require('../models/Business')
-
+const ErrorResponse = require('../utils/errorResponse')
 
 // @desc  Create a business
 //@route  POST /api/v1/business
@@ -20,4 +20,52 @@ exports.createBusiness = asyncHandler(async (req, res, next) => {
 //@access Public
 exports.getBusinesses = asyncHandler(async (req, res, next) => {
   res.status(200).json(res.advancedResults)
+})
+
+// @desc  Get business
+//@route  Get /api/v1/business/:id
+//@access Public
+exports.getBusiness = asyncHandler(async (req, res, next) => {
+
+  const business = await Business.findById(req.params.id)
+
+  if (!business) return next(new ErrorResponse(`Business not found with the id of ${req.params.id}`, 400))
+
+  res.status(200).json({
+    success: true,
+    data: business
+  })
+})
+
+// @desc  Update a business
+//@route  PUT /api/v1/business/:id
+//@access Private
+exports.updateBusiness = asyncHandler(async (req, res, next) => {
+
+  const business = await Business.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  })
+
+  if (!business) return next(new ErrorResponse(`No business with the id of ${req.params.id}`, 400))
+
+  res.status(201).json({
+    success: true,
+    data: business
+  })
+})
+
+// @desc  Delete a business
+//@route  DELETE /api/v1/business/:id
+//@access Private
+exports.deleteBusiness = asyncHandler(async (req, res, next) => {
+
+  const business = await Business.findByIdAndDelete(req.params.id)
+
+  if (!business) return next(new ErrorResponse(`No business with the id of ${req.params.id}`, 400))
+
+  res.status(200).json({
+    success: true,
+    data: {}
+  })
 })
--- routes/business.js
+++ routes/business.js
@@ -2,10 +2,15 @@ const express = require('express')
 const router = express.Router()
 const advancedResults = require('../middlewares/advancedResults')
 const Business = require('../models/Business')
-const { getBusinesses, createBusiness } = require('../controllers/business')
+const { getBusinesses, getBusiness, createBusiness, updateBusiness, deleteBusiness } = require('../controllers/business')
 
 router.route('/')
   .get(advancedResults(Business), getBusinesses)
   .post(createBusiness)
+
+router.route('/:id')
+  .get(getBusiness)
+  .put(updateBusiness)
+  .delete(deleteBusiness)
 
 module.exports = router
