0504b8c8c220907db98ab499f2a6941e622821b0
PetarVujetic <vujeticp@gmail.com>
2021-06-27 16:17:27 +0200

    business async fix and routes loaded in server

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
+
+// Load routes
+const businessRoute = require('./routes/business')
+
+
+// Set routes
+app.use('/api/v1/business', businessRoute)
+
 const server = app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold);
 })
